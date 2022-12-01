b46305cf67a5c7b2b44f82f557f614aeaf17ff63
BricePrs <brice.peres@gmail.com>
2022-12-01 14:11:37 +0100

    Creation/suppression de clients + tri des Restaurants

--- code/src/data/Clients.java
+++ code/src/data/Clients.java
@@ -1,10 +1,25 @@
 package data;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Clients {
 
     private static final String tableClients = "Clients";
+    private static final String tableIdClients = "IdentifiantsClient";
 
+
+
+    public void create(String email, String mdp, String nom, String prenom, String adresse) throws SQLException {
+        ResultSet result = Table.sendQuery(String.format("SELECT MAX(idClient) FROM %s", tableClients));
+        result.next();
+        int newId = result.getInt(1) + 1;
+        Table.sendQuery(String.format("INSERT INTO %s VALUES (%d, '%s', '%s', '%s', '%s')", tableClients, newId, mdp, nom, prenom, adresse));
+        Table.sendQuery(String.format("INSERT INTO %s VALUES (%d)", tableClients, newId));
+    }
+
+    public void remove(int idClient) {
+        Table.sendQuery(String.format("DELETE FROM %s WHERE idClient='%d'", tableClients, idClient));
+    }
 
 }
