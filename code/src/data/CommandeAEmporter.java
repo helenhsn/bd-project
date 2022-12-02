4c99824e4632f2df6308538b7ea05400dc40c000
colinotp <paul.colinot@grenoble-inp.org>
2022-12-02 19:35:19 +0100

    code Contenu des Commandes

--- code/src/data/CommandeAEmporter.java
+++ code/src/data/CommandeAEmporter.java
@@ -11,12 +11,20 @@ public class CommandeAEmporter {
 
     protected static final String tableCommandes = "Commandes";
     protected static final String tableClients = "Clients";
+    protected static final String tableContenu = "ContenuCommande";
 
 
-    public static void create(String emailRestaurant, String emailClient, int dateCommande, int heureCommande, int prixFinal, TypeStatut statut) throws SQLException {
+    public static void create(String emailRestaurant, String emailClient, int dateCommande, int heureCommande) throws SQLException {
         ResultSet idClient = Table.sendQuery(String.format("SELECT idClient FROM %s WHERE emailClient = %s", tableClients, emailClient));
 
-        Table.sendQuery(String.format("INSERT INTO %s VALUES ('%s', %d, %d, %d, %d, '%s')", tableCommandes, emailRestaurant, idClient, dateCommande, heureCommande, prixFinal, statut.toString()));
+        Table.sendQuery(String.format("INSERT INTO %s VALUES ('%s', %d, %d, %d, 0, 'AttenteConfirmation')", tableCommandes, emailRestaurant, idClient, dateCommande, heureCommande));
+    }
+
+    // Remplis la table ContenuCommande
+    public static void commande(String emailRestaurant, String emailClient, int dateCommande, int heureCommande, int idPlat, int quantite){
+        ResultSet idClient = Table.sendQuery(String.format("SELECT idClient FROM %s WHERE emailClient = %s", tableClients, emailClient));
+
+        Table.sendQuery(String.format("INSERT INTO %s VALUES ('%s', '%s', '%s', %d, %d, %d)", tableContenu, emailRestaurant, idPlat, idClient, dateCommande, heureCommande, quantite));
     }
 
     public static void setStatut(String emailRestaurant, String emailClient, int dateCommande, int heureCommande, TypeStatut statut){
@@ -24,6 +32,12 @@ public class CommandeAEmporter {
     }
 
     public static ResultSet get(String emailRestaurant, String emailClient, int dateCommande, int heureCommande, int idCommande, String attributs){
-        return Table.getAttributes(tableCommandes, attributs, String.format("idCommande = %d AND emailRestaurant = '%s' AND emailClient = '%s' AND dateCommande = %d AND heureCommande = %d", idCommande, emailRestaurant, emailClient, dateCommande, heureCommande) );
+        return Table.getAttributes(tableCommandes, attributs, String.format("emailRestaurant = '%s' AND emailClient = '%s' AND dateCommande = %d AND heureCommande = %d", emailRestaurant, emailClient, dateCommande, heureCommande));
+    }
+
+    
+    public static ResultSet contenu(String emailRestaurant, String emailClient, int dateCommande, int heureCommande){
+        return Table.getAttributes(tableContenu, "idPlat, quantite", String.format("emailRestaurant = '%s' AND emailClient = '%s' AND dateCommande = %d AND heureCommande = %d", emailRestaurant, emailClient, dateCommande, heureCommande));
+
     }
 }
